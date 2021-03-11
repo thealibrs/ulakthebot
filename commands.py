1ee703d469f9d326187301bd61b9359155be1d66
thealibrs <alibrs15@hotmail.com>
2021-03-11 17:02:36 +0200

    asdad

--- commands.py
+++ commands.py
@@ -1,4 +1,6 @@
 from telegram.ext import Updater
+from bs4 import BeautifulSoup
+import requests
 
 
 def start_command(update, context):
@@ -11,3 +13,18 @@ def wrong_command(update, context):
   message = "Hay aksi, kafam karıştı! Şimdilik yalnızca /start komutunu anlayabiliyorum"
 
   return update.message.reply_text(message)
+
+def get_news(update, context):
+     r = requests.get("https://www.haberler.com/son-dakika/")
+     soup = BeautifulSoup(r.content, "lxml")
+
+     news = soup.find_all('div', attrs={'class':"hblnBox"})
+     my_new = str()
+
+     for new in news[:3]:
+          time = new.find("div", attrs = {'class':'hblnTime'}).text
+          title = new.find('a', attrs={'class':"hblnTitle"}).get("title")
+          link = new.find('a', attrs={'class':"hblnTitle"}).get("href")
+  
+  print("Time   : {}\nTitle  : {}\nLink   : {}".format(time, title, link))
+  print("-"*20)
--- main.py
+++ main.py
@@ -9,8 +9,12 @@ def main():
     updater = Updater(TOKEN, use_context=True)
     dp = updater.dispatcher
 
+    #my commands
     dp.add_handler(CommandHandler("start", c.start_command))
+    dp.add_handler(CommandHandler("news", c.get_news))
 
+
+    #while I write unknown word that the bot doesnt know
     dp.add_handler(MessageHandler(Filters.text, f.wrong_command))
 
     updater.start_polling()
